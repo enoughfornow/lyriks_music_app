3dd5039828a70e18970e3726af2c52b4b8f1d170
Dmitry <enoughfornowbusiness@gmail.com>
2022-10-18 08:20:14 +0300

    refactor and create searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,32 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  // const [first, setfirst] = React.useState(second);
+
+  return (
+    <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+      <label htmlFor="search-field" className="sr-only">
+        Search all songs
+      </label>
+      <div className="flex justify-start items-center">
+        <FiSearch className="w-5 h-5 ml-4" />
+        <input
+          type="search"
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          value=""
+          onChange={() => {}}
+          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default Searchbar;
